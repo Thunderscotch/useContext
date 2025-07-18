ec113b57988861bb1adabf406fc295bb50991b3c
Thunderscotch <subashnatrayan28@gmail.com>
2025-07-19 00:08:35 +0530

    local storage update

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode, useEffect } from "react";
 
 type User ={
     firstName : string,
@@ -26,11 +26,25 @@ export const userContext = createContext<userContextType>({
 });
 
 export const Provider = ({children} : {children : ReactNode}) => {
-    const [user, setUser] = useState<User>({
-        firstName : "",
-        lastName : "",
-        email: ""
+    // const [user, setUser] = useState<User>({
+    //     firstName : "",
+    //     lastName : "",
+    //     email: ""
+    // })
+    const [user ,setUser] = useState<User>(() => {
+        const savedUser = localStorage.getItem("user");
+        return savedUser ? JSON.parse(savedUser) : {
+            firstName : "",
+            lastName : "",
+            email: ""
+        }
     })
+
+    useEffect(() => {
+        if(typeof window !== "undefined") {
+        localStorage.setItem("user", JSON.stringify(user))
+        }
+    },[user])
     
     return (
         <userContext.Provider value={{user, setUser}}>
